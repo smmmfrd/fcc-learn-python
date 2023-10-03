37578acebac7d05a8d2dba47dd96c495ac0a4549
smmmfrd <smmmfrd@gmail.com>
2023-10-02 22:59:26 -0700

    Showoff New Operators

---
+++ 02-operators/operators.py
@@ -0,0 +1,20 @@
+print("***** WEIRD OPERATORS *****")
+
+# Prints 5.0 because division creates a float.
+print("25 / 5" + "\t= " + str(25 / 5) + "\t(raw division)")
+
+print("24 / 5" + "\t= " + str(round(24 / 5)) + "\t(rounded)")
+
+print("24 / 5" + "\t= " + str(24 % 5) + "\t(remainder)")
+
+print("2 ^ 3" + "\t= " + str(2**3) + "\t(power of)")
+
+print("***** BOOLEAN OPERATORS *****")
+
+print("4.0 == 4" + "\t= " + str(4.0 == 4))
+
+print("not True" + "\t= " + str(not True))
+
+print("True and False" + "\t= " + str(True and False))
+
+print("True or False" + "\t= " + str(True or False))
