43dee258d058d9b66a988f80c1f7bceb7cc844ce
smmmfrd <smmmfrd@gmail.com>
2023-10-04 17:45:56 -0700

    Nested Dictionaries

--- 06-dicts/dictionaries.py
+++ 06-dicts/dictionaries.py
@@ -68,3 +68,16 @@ print(band)
 print(band3)
 
 # Nested dictionaries
+
+member1 = {
+    "name": "Plant",
+    "instrument": "vocals",
+}
+member2 = {
+    "name": "Page",
+    "instrument": "guitar",
+}
+band = {"member1": member1, "member2": member2}
+
+print(band)
+print(band["member1"]["name"])
