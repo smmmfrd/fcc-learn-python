aa4b83b0710b0de94e5141246f3aaf0fc96188d4
smmmfrd <smmmfrd@gmail.com>
2023-10-05 12:34:14 -0700

    Loops

---
+++ 07-loops/loops.py
@@ -0,0 +1,54 @@
+value = 0
+
+# while value <= 10:
+#     print(value)
+
+#     if value == 5:
+#         break
+
+#     value += 1
+
+# while value <= 10:
+#     value += 1
+
+#     if value == 5:
+#         continue
+
+#     print(value)
+# else:  # Fires when statement is false. (at end?)
+#     print(value)
+
+names = ["Dave", "Sara", "John"]
+# for name in names:
+#     print(name)
+
+# for char in "Mississippi":
+#     print(char)
+
+# for name in names:
+#     if name == "Sara":
+#         break
+#     print(name)
+
+# for name in names:
+#     if name == "Sara":
+#         continue
+#     print(name)
+
+# for x in range(4):
+#     print(x)
+
+# Start 2 and end 4
+# for x in range(2, 4):
+#     print(x)
+
+# for x in range(5, 101, 5):
+#     print(x)
+# else:
+#     print("it's joever")
+
+actions = ["codes", "eats", "sleeps"]
+
+for name in names:
+    for action in actions:
+        print(name + " " + action + ".")
