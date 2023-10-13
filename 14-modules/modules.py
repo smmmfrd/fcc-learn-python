449e7d2b149112ac02e6151da75b2ad3d27b10bc
smmmfrd <smmmfrd@gmail.com>
2023-10-13 15:48:54 -0700

    Module Lesson

---
+++ 14-modules/modules.py
@@ -0,0 +1,17 @@
+from math import pi		# Only import one thing
+import random as rdm	# Rename module
+import kansas
+
+print(pi)
+
+print(rdm.random())
+
+# Print all things in directory
+# for item in dir(rdm):
+# 	print(item)
+
+print(kansas.capital)
+kansas.randomfunfact()
+
+print(__name__)			# Prints __main__ (the file being ran)
+print(kansas.__name__)	# Prints kansas (name of file)
