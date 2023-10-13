170c967ab60f30c9c72939f79f762179e229c726
smmmfrd <smmmfrd@gmail.com>
2023-10-13 15:52:40 -0700

    RPS Module

--- 14-modules/modules.py
+++ 14-modules/modules.py
@@ -1,6 +1,7 @@
 from math import pi		# Only import one thing
 import random as rdm	# Rename module
 import kansas
+from rps7 import rock_paper_scissors
 
 print(pi)
 
@@ -15,3 +16,5 @@ kansas.randomfunfact()
 
 print(__name__)			# Prints __main__ (the file being ran)
 print(kansas.__name__)	# Prints kansas (name of file)
+
+rock_paper_scissors()
